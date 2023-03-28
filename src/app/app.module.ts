b6de2704e905016cadccab1dbfa1e9508c181d38
houdaguirat <gkhouda84@gmail.com>
2023-03-28 13:17:48 +0200

    final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FruitsRoutingModule } from './fruits/fruits-routing.module';
+import { FruitsModule } from './fruits/fruits.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FruitsRoutingModule,
+    HttpClientModule,
+    FruitsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
