329603033733f376ab1070798512d07e4948fd91
houdaguirat <gkhouda84@gmail.com>
2023-04-01 12:33:33 +0200

    new version

--- src/app/fruits/fruits.service.ts
+++ src/app/fruits/fruits.service.ts
@@ -8,19 +8,19 @@ export class FruitsService {
 
   constructor(private http: HttpClient) {}
   getAll() {
-    return this.http.get<Fruits[]>('http://localhost:3000/fruits');
+    return this.http.get<Fruits[]>('http://localhost:8080/Fruit');
   }
   create(payload: Fruits) {
-    return this.http.post<Fruits>('http://localhost:3000/fruits', payload);
+    return this.http.post<Fruits>('http://localhost:8080/Fruit', payload);
   }
   getById(id: number) {
-    return this.http.get<Fruits>(`http://localhost:3000/fruits/${id}`);
+    return this.http.get<Fruits>(`http://localhost:8080/Fruit/${id}`);
    }
     
    update(payload:Fruits){
-    return this.http.put(`http://localhost:3000/fruits/${payload.id}`,payload);
+    return this.http.put(`http://localhost:8080/Fruit`,payload);
    }
    delete(id:number){
-    return this.http.delete<Fruits>(`http://localhost:3000/fruits/${id}`);
+    return this.http.delete<Fruits>(`http://localhost:8080/Fruit/${id}`);
  }
 }
